bfc82da9303aac30c51b62adfdc6dbd495a22695
bereket <bereketsiraw123@gmail.com>
2023-03-20 12:54:26 +0400

    search_bar and zoom in and zoom out 3/20

--- PWA/static/gesture.js
+++ PWA/static/gesture.js
@@ -35,22 +35,13 @@ function zoomOut(ele){
 const canvas_container = document.querySelector('.row');
 let Rectangle = MindFusion.Drawing.Rect;
 let rect = null;
-let d1 = 1, rs = 1, rf = 0;
+let d1 = 100, rs = 1, rf = 0;
 
  function dist(a) {
     var zw = a.touches[0].pageX - a.touches[1].pageX, zh = a.touches[0].pageY - a.touches[1].pageY;
     return Math.sqrt(zw * zw + zh * zh);
   }
-  // newRect = new RectangleF(mousePos.X- visible.Size.Width / 2, mousePos.Y - visible.Size.Height / 2, mousePos.X + visible.Size.Width / 2, mousePos.Y + visible.Size.Height / 2);
-  // if (e.Delta > 0)
-  // {
-  //    newRect.Inflate(1.5f, 1.5f);
-  // }
-  // else
-  // {
-  //    newRect.Inflate(-1.5f, -1.5f);
-  // }
-  //attach the events
+
   canvas_container.addEventListener('touchstart', function(event) {
     event.preventDefault();
     if (event.touches.length > 1) {
@@ -64,15 +55,6 @@ let d1 = 1, rs = 1, rf = 0;
 
       //get the ratio
       rf = dist(event) / d1 * rs;
-      // X = event.touches[0].pageX + event.touches[1].pageX;
-      // X /= 2;
-      // Y = event.touches[0].pageY + event.touches[1].pageY;
-      // Y /= 2;
-
-      // let visible_w = diagram._element.parentNode.clientWidth;
-      // let visible_h = diagram._element.parentNode.clientHeight;
-
-      // rect = new Rectangle(X - visible_w/ 2, Y - visible_h/ 2, X + visible_w / 2, Y + visible_h / 2);
       with(Math){
         // console.log('pt: ', rect);
         map = min(max(1, rf), 200);
@@ -85,10 +67,12 @@ let d1 = 1, rs = 1, rf = 0;
 
   //check if scale is less than 1 and keep the previous ratio
   canvas_container.addEventListener('touchend', function(e) {
+    e.preventDefault();
     rs = (rf < 1)? 1: rf;
   });
 
   canvas_container.addEventListener('touchcancel', function(e){
+    e.preventDefault();
     rs = (rf < 1)? 1: rf;
   });
 
